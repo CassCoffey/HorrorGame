41e7f70bbb3acfdf86018fd3fa1848999d6b0d86
Sean Coffey <sean@coffeyhouse.com>
2014-12-02 16:17:55 -0500

    Added interpolation to player script

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,6 +4,11 @@ using System.Collections;
 public class Player : MonoBehaviour {
 
     public float speed = 10f;
+    private float lastSynchronizationTime = 0f;
+    private float syncDelay = 0f;
+    private float syncTime = 0f;
+    private Vector3 syncStartPosition = Vector3.zero;
+    private Vector3 syncEndPosition = Vector3.zero;
 
     void Update()
     {
@@ -11,20 +16,35 @@ public class Player : MonoBehaviour {
         {
             InputMovement();
         }
+        else
+        {
+            SyncedMovement();
+        }
     }
 
     void OnSerializeNetworkView(BitStream stream, NetworkMessageInfo info)
     {
         Vector3 syncPosition = Vector3.zero;
+        Vector3 syncVelocity = Vector3.zero;
         if (stream.isWriting)
         {
             syncPosition = rigidbody.position;
             stream.Serialize(ref syncPosition);
+
+            syncVelocity = rigidbody.velocity;
+            stream.Serialize(ref syncVelocity);
         }
         else
         {
             stream.Serialize(ref syncPosition);
-            rigidbody.position = syncPosition;
+            stream.Serialize(ref syncVelocity);
+
+            syncTime = 0f;
+            syncDelay = Time.time - lastSynchronizationTime;
+            lastSynchronizationTime = Time.time;
+
+            syncEndPosition = syncPosition + syncVelocity * syncDelay;
+            syncStartPosition = rigidbody.position;
         }
     }
 
@@ -41,5 +61,11 @@ public class Player : MonoBehaviour {
 
         if (Input.GetKey(KeyCode.A))
             rigidbody.MovePosition(rigidbody.position - Vector3.right * speed * Time.deltaTime);
+    }
+
+    private void SyncedMovement()
+    {
+        syncTime += Time.deltaTime;
+        rigidbody.position = Vector3.Lerp(syncStartPosition, syncEndPosition, syncTime / syncDelay);
     }
 }
