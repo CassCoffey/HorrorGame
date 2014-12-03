3ba8ffaf3e4d66ac50e83d337fa3860926ff425d
Sean Coffey <sean@coffeyhouse.com>
2014-12-03 15:16:01 -0500

    Networked the player camera.

---
+++ Assets/Scripts/NetworkCamera.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NetworkCamera : MonoBehaviour {
+
+    void OnNetworkInstantiate()
+    {
+        if (networkView.isMine)
+        {
+            GetComponent<Camera>().enabled = true;
+        }
+        else
+        {
+            GetComponent<Camera>().enabled = false;
+        }
+    }
+}
