857b8a45859890b3936298413215bb351581f971
Jflinchum <jonflinchum@gmail.com>
2014-12-07 22:33:20 -0500

    Tweaked Library Manager

--- Assets/Scripts/Menus/LobbyManager.cs
+++ Assets/Scripts/Menus/LobbyManager.cs
@@ -29,13 +29,8 @@ public class LobbyManager : MonoBehaviour {
         }
 	}
 
-	void OnPlayerConnected(NetworkPlayer player) {
-		Debug.Log ("Player Connected!" + player.ipAddress);
-		for (int i = 0; i < playerList.Count; i++)
-		{
-			Destroy(playerList[i]);
-		}
-		playerList.Clear();
+	void UpdatePlayerList(NetworkPlayer player) {
+		Debug.Log (Network.connections);
 		for (int i = 0; i < Network.connections.Length; i++)
 		{
 			GameObject label = (GameObject)Instantiate(labelPrefab);
@@ -51,25 +46,23 @@ public class LobbyManager : MonoBehaviour {
 		}
 	}
 
+	void ClearPlayerList(){
+		for (int i = 0; i < playerList.Count; i++)
+		{
+			Destroy(playerList[i]);
+		}
+		playerList.Clear();
+	}
+
+	void OnPlayerConnected(NetworkPlayer player) {
+		Debug.Log ("Player Connected!" + player.ipAddress);
+		ClearPlayerList ();
+		UpdatePlayerList(player);
+	}
+
 	void OnPlayerDisconnected(NetworkPlayer player) {
 		Debug.Log ("Player Disconnected!" + player.ipAddress);
-		for (int i = 0; i < playerList.Count; i++) 
-		{
-			Destroy (playerList [i]);
-		}
-		playerList.Clear ();
-		for (int i = 0; i < Network.connections.Length; i++) 
-		{
-			GameObject label = (GameObject)Instantiate (labelPrefab);
-			label.transform.SetParent (playerListPanel.transform, false);
-			UnityEngine.UI.Button labelScript = label.GetComponent<UnityEngine.UI.Button> ();
-			label.GetComponentInChildren<Text> ().font = font;
-			label.GetComponentInChildren<Text> ().text = (player.ipAddress);
-			label.GetComponent<RectTransform> ().anchorMax = new Vector2 (1, 1 - ((1.0f / (float)Network.connections.Length) * i));
-			label.GetComponent<RectTransform> ().anchorMin = new Vector2 (0, (1 - (1.0f / (float)Network.connections.Length)) - ((1.0f / (float)Network.connections.Length) * i));
-			label.GetComponent<RectTransform> ().offsetMax = new Vector2 (0, 0);
-			label.GetComponent<RectTransform> ().offsetMin = new Vector2 (0, 0);
-			playerList.Add (label);
-		}
+		ClearPlayerList ();
+		UpdatePlayerList (player);
 	}
 }
