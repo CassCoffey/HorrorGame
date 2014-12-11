938201de53823bd4a95e4e323dc156c52961a60c
Sean Coffey <sean@coffeyhouse.com>
2014-12-10 23:10:37 -0500

    Moved all the files. Added spawnpoints.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -6,15 +6,22 @@ public class GameManager : MonoBehaviour {
     // The default player prefab.
     public GameObject playerPrefab;
 
+    private GameObject[] spawnPoints;
+
 	// Used when the player enters the map to spawn a player object.
-	void Start () {
+	void Start () 
+    {
+        spawnPoints = GameObject.FindGameObjectsWithTag("Respawn");
         SpawnPlayer();
 	}
 
     // Create a player object.
     private void SpawnPlayer()
     {
+        int index = Random.Range(0, spawnPoints.Length);
+        Vector3 spawnPoint = spawnPoints[index].transform.position;
+        Vector3 spawn = new Vector3(spawnPoint.x + Random.Range(-5, 5), spawnPoint.y, spawnPoint.z + Random.Range(-5, 5));
         Debug.Log("Spawning Player");
-        GameObject player = (GameObject)Network.Instantiate(playerPrefab, new Vector3(0f, 2f, 0f), Quaternion.identity, 0);
+        GameObject player = (GameObject)Network.Instantiate(playerPrefab, spawn, Quaternion.identity, 0);
     }
 }
