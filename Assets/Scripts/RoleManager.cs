1bc240418884fedff96ff0ba60c516f4b907a3ca
Jflinchum <jonflinchum@gmail.com>
2015-01-02 18:33:19 -0500

    Added RoleManager script that doesn't do much yet. Just creates a list of all the players connected and removes them from that list one by one.

---
+++ Assets/Scripts/RoleManager.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class RoleManager : MonoBehaviour {
+
+	public GameObject RoleNameText;
+	public GameObject RoleDescriptionText;
+	private List<NetworkPlayer> playerList = new List<NetworkPlayer>();
+
+	// Use this for initialization
+	void Start () {
+		playerList.Add (Network.player);
+		for (int i = 0; i < Network.connections.Length; i++) 
+		{
+			playerList.Add (Network.connections[i]);
+		}
+		Debug.Log (playerList);
+		while (playerList.Count != 0) 
+		{
+			int index = Random.Range(0,playerList.Count);
+
+			playerList.RemoveAt(index);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
