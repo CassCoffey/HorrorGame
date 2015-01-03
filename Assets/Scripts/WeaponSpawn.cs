6fd1f1d8f3f3d92c549975ab14ec6b6095da3dcd
Sean Coffey <sean@coffeyhouse.com>
2015-01-03 16:13:51 -0500

    Began work on weapon spawning

---
+++ Assets/Scripts/WeaponSpawn.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WeaponSpawn : MonoBehaviour {
+
+    public GameObject[] weapons;
+
+	// Use this for initialization
+	void Start () {
+	    
+	}
+}
