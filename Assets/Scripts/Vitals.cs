e9a0b61f8ab1f2ec2ad060e560ec1a4a4b32d8d1
Sean Coffey <sean@coffeyhouse.com>
2015-01-04 18:12:25 -0500

    Added health bar.

--- Assets/Scripts/Vitals.cs
+++ Assets/Scripts/Vitals.cs
@@ -1,4 +1,6 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
 using System.Collections;
 
 public class Vitals : MonoBehaviour {
@@ -6,11 +8,16 @@ public class Vitals : MonoBehaviour {
     public int health;
     public float stamina;
     public int scent;
+    public GameObject vitalsPanel;
 
     public void TakeDamage(int damage)
     {
         Debug.Log("Taking Damage");
         health -= damage;
+        if (networkView.isMine)
+        {
+            vitalsPanel.transform.FindChild("HealthSlider").GetComponent<Slider>().value = health;
+        }
         if (health <= 0)
         {
             Debug.Log("You died.");
