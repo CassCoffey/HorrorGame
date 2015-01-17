ba4e7081b2b782ea73052379be025dadd65a5088
Sean Coffey <sean@coffeyhouse.com>
2015-01-17 15:21:19 -0500

    Comments and cleanup in WeaponLocScript

--- Assets/Scripts/Game/WeaponLocScript.cs
+++ Assets/Scripts/Game/WeaponLocScript.cs
@@ -3,12 +3,18 @@ using System.Collections;
 
 public class WeaponLocScript : MonoBehaviour {
 
+    /// <summary>
+    /// Activates the weapon's trigger.
+    /// </summary>
 	public void TriggerWeapon()
     {
         transform.GetChild(0).collider.isTrigger = true;
         transform.GetChild(0).collider.enabled = true;
     }
 
+    /// <summary>
+    /// Puts the weapon back in rest state.
+    /// </summary>
     public void RestWeapon()
     {
         transform.GetChild(0).collider.enabled = false;
