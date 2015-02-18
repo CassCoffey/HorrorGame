ad5c7f8b76f5600c8dc35bfce15b2e3658345317
Sean Coffey <sean@coffeyhouse.com>
2015-02-17 19:15:04 -0500

    Deletes networkmanager on switching back to lobby.

--- Assets/Scripts/Menus/ResultsManager.cs
+++ Assets/Scripts/Menus/ResultsManager.cs
@@ -63,6 +63,8 @@ public class ResultsManager : MonoBehaviour {
 	
 	public void GoToLobby()
 	{
+        Destroy(GameObject.Find("NetworkManager"));
+        Network.SetLevelPrefix(0);
 		Application.LoadLevel ("MainMenu");
 		if (Network.isServer) 
 		{
